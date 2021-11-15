8d1de2419c8a88dfafaa22d28e155f6f38cc91d5
jmrosenberger <jmrosenberger0811@yahoo.com>
2021-11-15 15:30:30 -0600

    fixed authentication, can now log in

--- rareapi/admin.py
+++ rareapi/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from rareapi.models import RareUser, Post, Category
 
 # Register your models here.
+
+admin.site.register(RareUser)
+admin.site.register(Post)
+admin.site.register(Category)
